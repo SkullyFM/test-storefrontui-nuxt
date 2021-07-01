2ac2a61cfce8ccc6049834669bc617f3e8b3f42f
SkullyFM <franck.mahon@gmail.com>
2021-07-01 23:32:27 +0200

    Basic SfCallToAction with Nuxt & Storefront UI showing a discrepancy in styles between the dist folder and "yarn dev"

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <SfCallToAction
+    title="Subscribe to Newsletters"
+    buttonText="Subscribe"
+    link="/"
+    description="Be aware of upcoming sales and events. Receive gifts and special offers!"
+    background="#e1e3e2"
+    image=""
+  />
+</template>
+
+<script>
+import { SfCallToAction } from "@storefront-ui/vue";
+export default {
+  components: {
+    SfCallToAction
+  }
+}
+</script>
